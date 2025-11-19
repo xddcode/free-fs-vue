6d71bcadcc9543e8ed0645d3a9929d1d76042cbd
yanzy <1319542051@qq.com>
2025-11-19 23:10:53 +0800

    update 前端vite配置dev改成代理模式

--- config/vite.config.dev.ts
+++ config/vite.config.dev.ts
@@ -1,15 +1,27 @@
-import { mergeConfig } from 'vite';
+import { mergeConfig, loadEnv, defineConfig } from 'vite'; // 引入 loadEnv 和 defineConfig
 import eslint from 'vite-plugin-eslint';
 import baseConfig from './vite.config.base';
 
-export default mergeConfig(
-  {
-    mode: 'development',
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  const apiPrefix = env.VITE_API_BASE_URL;
+
+  const currentConfig = {
+    mode: 'development', // 注意：如果是 build 模式，Vite 会自动覆盖这个值，这里写死也没事，但建议删掉让 Vite 自动推断
     server: {
       host: '0.0.0.0',
       open: true,
       fs: {
         strict: true,
+      },
+      // 3. 动态代理配置
+      proxy: {
+        [apiPrefix]: {
+          target: 'http://localhost:8080',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(new RegExp(`^${apiPrefix}`), ''),
+        },
       },
     },
     plugins: [
@@ -19,6 +31,8 @@ export default mergeConfig(
         exclude: ['node_modules'],
       }),
     ],
-  },
-  baseConfig
-);
+  };
+
+  // 3. 合并基础配置并返回
+  return mergeConfig(baseConfig, currentConfig);
+});
